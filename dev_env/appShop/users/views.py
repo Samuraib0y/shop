289c6c245b3ed7b7269bd3bdee987712ae4507cb
Samuraib0y <samurai.pripa@gmail.com>
2025-01-30 16:12:01 +0200

    add carts(Корзина), reworl cart module

--- dev_env/appShop/users/views.py
+++ dev_env/appShop/users/views.py
@@ -70,7 +70,10 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+def users_cart(request):
+    return render(request, 'users/users-cart.html')
 
+@login_required
 def logout(request):
     messages.success(request, f'{request.user.username}, Вы вышли из аккаунта!')
     auth.logout(request)
